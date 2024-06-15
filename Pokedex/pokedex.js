a10fc35fb25cb1422f77f97c43fc2238be291ca7
anitaSF <ana.isabel.santos@bootcamp-upgrade.com>
2024-06-15 21:05:47 +0200

    practica pokedex

--- Pokedex/pokedex.js
+++ Pokedex/pokedex.js
@@ -1,18 +1,30 @@
 console.log('Pokeapi!');
 
 const pokeList = document.querySelector('#pokedex');
-console.log(pokeList);
 
 function createPoke(pokemon) {
+    const li = document.createElement('li');
 
+    li.innerHTML = `
+    <div class="poke-card">
+        <img class="image-poke" src="${pokemon.sprites['front_default']}" >
+        <h2 class="name-poke">${pokemon.name}</h2>
+        <h4 class="tipo-poke">${pokemon.types.map((type) => type.type.name).join(' - ')}</h4>
+    </div>
+    `;
+
+    pokeList.appendChild(li);
 }
 
 async function getPoke(id) {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const data = await response.json();
-        console.log(data);
 
+        const pokemon = data;
+        console.log(pokemon);
+
+        createPoke(pokemon);
 
     } catch (error) {
         console.log(error);
